835d5831425d5ca91a46fb89ce648fea4c7393f5
Ammonh <ammonhenderson@gmail.com>
2018-10-23 11:41:34 -0500

    Update HttpTrigger/index.js

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,10 +1,10 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.query.productid || (req.body && req.body.productid)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello " + (req.query.productid || req.body.productid)
         };
     }
     else {
